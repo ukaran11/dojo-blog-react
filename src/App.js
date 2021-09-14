1e4f1b30044412e9e77aa5f79642f9496f0a7c2a
ukaran11 <ukaran11@gmail.com>
2021-09-14 19:12:01 +0530

    Utsav | Added Navbar and Home Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>App Content</h1>
+        <Home/>
       </div>
     </div>
   );
