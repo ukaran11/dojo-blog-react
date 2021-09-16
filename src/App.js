ba0ed286125880a212ed75cb42f628eadba34dfc
ukaran11 <ukaran11@gmail.com>
2021-09-16 16:07:51 +0530

    Utsav | Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Home from './Home';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Route path="/">
+            <Home/>
+          </Route>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
