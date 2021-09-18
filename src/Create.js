93c2c4143453611ac887a010fd0eec34d8324570
ukaran11 <ukaran11@gmail.com>
2021-09-18 22:42:59 +0530

    Utsav | Added submit handler function

--- src/Create.js
+++ src/Create.js
@@ -4,10 +4,16 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+    }
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
@@ -30,9 +36,6 @@ const Create = () => {
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
-                <p>{ title }</p>
-                <p>{ body }</p>
-                <p>{ author }</p>
             </form>
         </div>
     );
